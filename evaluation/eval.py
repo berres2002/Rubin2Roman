a331b9a0cde614d98267eaf09e8d0372a9a59876
berres2002 <berres2002@gmail.com>
2026-05-07 19:19:02 -0400

    moved data processing files and added eval.py

---
+++ evaluation/eval.py
@@ -0,0 +1,133 @@
+import numpy as np
+from skimage.metrics import peak_signal_noise_ratio, structural_similarity
+import sep
+from photutils.profiles import CurveOfGrowth
+from photutils.centroids import centroid_2dg
+
+def _FindCenterPeak(image):
+    cenxy = centroid_2dg(image)
+    return cenxy
+
+def GetCenterPeak(images):
+    n, h, w = images.shape
+    cenxy = np.zeros((n,2))
+    for ii in range(n):
+        cenxy[ii] = _FindCenterPeak(images[ii])
+    return cenxy
+
+def normalize_unit(image):
+    """Normalize the input image to be between 0 and 1."""
+    im_norm = abs(image.min(axis=(1,2), keepdims=True)) + image
+    im_norm = im_norm / im_norm.max(axis=(1,2), keepdims=True)
+    return im_norm
+
+def ZScoreNormalize(image: np.ndarray) -> np.ndarray:
+    """
+    Z-score normalize each channel of the input image independently.
+    
+    Parameters:
+    image (np.ndarray): Input image of shape (C, H, W) where C is the number of channels.
+    
+    Returns:
+    np.ndarray: Z-score normalized image of the same shape as the input.
+    """
+    mean = np.mean(image, axis=(1, 2), keepdims=True)
+    std = np.std(image, axis=(1, 2), keepdims=True)
+    return (image - mean) / std
+
+# taken from BTK reconstruction eval
+def psnr(images1: np.ndarray, images2: np.ndarray) -> np.ndarray:
+    """Compute peak-signal-to-noise-ratio from skimage.
+
+    Args:
+        images1: Array of shape `NHW` containing `N` images each of
+                size `NHW`.
+        images2: Array of shape `NHW` containing `N` images each of
+                size `NHW`.
+
+    Returns:
+        Returns PSNR between each corresponding iamge as an array of shape `N`.
+    """
+    n, h, w = images1.shape
+    assert images1.min() >= 0 and images2.min() >= 0
+    assert (n, h, w) == images2.shape
+    psnr_ = np.zeros(n)
+    for ii in range(n):
+        im1 = images1[ii] / images1[ii].max()
+        im2 = images2[ii] / images2[ii].max()
+        psnr_[ii] = peak_signal_noise_ratio(im1, im2, data_range=1)
+    return psnr_
+
+
+def struct_sim(images1: np.ndarray, images2: np.ndarray, **kwargs) -> np.ndarray:
+    """Compute structural similarity index from skimage.
+
+    By default, we normalize the images to be between 0 and 1. So that the
+    `data_range` is 1.
+
+    Args:
+        images1: Array of shape `NHW` containing `N` images each of
+                        size `NHW`.
+        images2: Array of shape `NHW` containing `N` images each of
+                        size `NHW`.
+        kwargs: Keyword arguments to be passed in to `peak_signal_noise_ratio` function
+                within `skimage.metrics`.
+
+    Returns:
+        Returns structural similarity index between each corresponding iamge as
+        an array of shape `N`.
+    """
+    assert images1.min() >= 0 and images2.min() >= 0
+    n, h, w = images1.shape
+    assert (n, h, w) == images2.shape
+    ssim = np.zeros(n)
+    if 'full' in kwargs:
+        Sims = np.zeros_like(images1)
+    for ii in range(n):
+        im1 = images1[ii] / images1[ii].max()
+        im2 = images2[ii] / images2[ii].max()
+        if 'full' in kwargs:
+            ssim, S= structural_similarity(im1, im2, data_range=1, **kwargs)
+            ssim[ii] = ssim
+            Sims[ii] = S
+        else:
+            ssim[ii] = structural_similarity(im1, im2, data_range=1, **kwargs)
+    if 'full' in kwargs:
+        return ssim, Sims
+    else:
+        return ssim
+    
+# TODO: add aperture photometry metric
+def _get_single_aperture_flux(image, x, y, radius, sky_level=None):
+    """Compute the flux within a circular aperture centered at (x, y) with the given radius."""
+    assert image.ndim == 2
+    # assert x.ndim == 1 and y.ndim == 1
+    flux, fluxerr, _ = sep.sum_circle(image, [x], [y], radius, var=sky_level)
+    return flux, fluxerr
+
+def get_aperture_fluxes(images, x, y, radius, sky_level=None):
+    """Compute the flux within a circular aperture centered at (x, y) with the given radius for each image."""
+    n, h, w = images.shape
+    assert x.shape == (n,) and y.shape == (n,)
+    fluxes = np.zeros(n)
+    fluxerrs = np.zeros(n)
+    for ii in range(n):
+        fluxes[ii], fluxerrs[ii] = _get_single_aperture_flux(images[ii], x[ii], y[ii], radius, sky_level)
+    return fluxes, fluxerrs
+
+# TODO: add half-light radius metric
+def _compute_halfLightR(image, xycen, radii=np.linspace(1,25,50), error=None):
+    """Compute the half-light radius in pixels of a single image given the center and a set of radii."""
+    
+    cog = CurveOfGrowth(image,xycen,radii,error=error)
+    cog.normalize(method='max')
+    hfc = abs(cog.profile-0.5).argmin()
+    return cog.radius[hfc]
+
+def get_HLR(images, x, y):
+    n, h, w = images.shape
+    assert x.shape == (n,) and y.shape == (n,)
+    hlrs = np.zeros(n)
+    for ii in range(n):
+        hlrs[ii] = _compute_halfLightR(images[ii], (x[ii], y[ii]))
+    return hlrs
