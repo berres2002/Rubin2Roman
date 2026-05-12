04d1cc90959355fe8a177d1c85bcccdc5245f301
berres2002 <berres2002@gmail.com>
2026-05-12 11:19:58 -0500

    fixed bugs and added print statements

--- evaluation/run_sample_eval.py
+++ evaluation/run_sample_eval.py
@@ -1,7 +1,7 @@
 from score_models.score_model import ScoreModel
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import numpy as np
-from astropy.visualization import ZScaleInterval
+#from astropy.visualization import ZScaleInterval
 import pandas as pd
 import torch
 from tqdm import tqdm
@@ -21,7 +21,9 @@ from eval import *
 #     std = np.std(image, axis=(1, 2), keepdims=True)
 #     return (image - mean) / std
 
-COLUMNS = ['cutout_id', 'psnr', 'ssim','roman_flux_Y','roman_flux_J','roman_flux_H','pred_flux_Y','pred_flux_J','pred_flux_H','roman_HLR_Y','roman_HLR_J','roman_HLR_H','pred_HLR_Y','pred_HLR_J','pred_HLR_H','lsst_flux_u','lsst_flux_g','lsst_flux_r','lsst_flux_i','lsst_flux_z','lsst_flux_y']
+COLUMNS = ['cutout_id', 'psnr_Y', 'psnr_J', 'psnr_H', 'ssim_Y', 'ssim_J', 'ssim_H','roman_flux_Y','roman_flux_J','roman_flux_H','pred_flux_Y','pred_flux_J','pred_flux_H','roman_HLR_Y','roman_HLR_J','roman_HLR_H','pred_HLR_Y','pred_HLR_J','pred_HLR_H',\
+        #    'lsst_flux_u','lsst_flux_g','lsst_flux_r','lsst_flux_i','lsst_flux_z','lsst_flux_y'\
+           ]
 
 def init_argparse():
     import argparse
@@ -38,30 +40,32 @@ if __name__ == "__main__":
     # checkpoints_directory = '/work/hdd/bfpq/aberres2/checkpoints/demo_cond_10k'
     dd1 = {col:[] for col in COLUMNS}
     checkpoints_directory = args.checkpoints_directory
+    print('Loading model...')
     model = ScoreModel(checkpoints_directory=checkpoints_directory)
 
     # df = pd.read_csv('/work/hdd/bfpq/aberres2/brightest_gals_cutouts_64/test1.csv')
+    print('Loading test data paths...')
     df = pd.read_csv(args.test_csv)
 
-    paths= df['path'].values
-    img_names = df['img'].values
+    paths= np.array(df['path'].values)
+    # img_names = df['img'].values
     # randomize the order of the paths
     np.random.seed(42069)
     np.random.shuffle(paths)
 
     #TEST CASE
     paths = paths[:1]
-
+    print('Evaluating model on test data...')
     for i in tqdm(range(len(paths))):
         path = paths[i]
         img = np.load(path)
-        name = img_names[i].strip('.npy')
+        name = path.split('/')[-1].strip('.npy')
         dd1['cutout_id'].append(name)
         fimg = ZScoreNormalize(img)
         nimg = fimg[:6] # First 6 channels as the conditioning image
         # duplicate to n_samples batches
         nimg = np.repeat(nimg[np.newaxis, :], args.n_samples, axis=0)
-        assert nimg.ndims == 4
+        assert nimg.ndim == 4
         cims = torch.from_numpy(nimg)
         # cims = cims.unsqueeze(0)
         cims = cims.to(model.device)
@@ -72,11 +76,15 @@ if __name__ == "__main__":
         s_median = np.median(full_samp, axis=0)
         # shape is (3, 64, 64) for the median image
         out1 = normalize_unit(s_median)
-        im_norm = normalize_unit(nimg[6:]) # Normalize the target image (last 3 channels)
+        im_norm = normalize_unit(fimg[6:]) # Normalize the target image (last 3 channels)
         psnr_val = psnr(out1, im_norm)
-        dd1['psnr'].append(psnr_val)
+        dd1['psnr_Y'].append(psnr_val[0])
+        dd1['psnr_J'].append(psnr_val[1])
+        dd1['psnr_H'].append(psnr_val[2])
         ssim_val = struct_sim(out1, im_norm)
-        dd1['ssim'].append(ssim_val)
+        dd1['ssim_Y'].append(ssim_val[0])
+        dd1['ssim_J'].append(ssim_val[1])
+        dd1['ssim_H'].append(ssim_val[2])
         out_cens = GetCenterPeak(out1)
         im_cens = GetCenterPeak(im_norm)
         im_hlr = get_HLR(im_norm,im_cens[:,0],im_cens[:,1])
@@ -98,3 +106,4 @@ if __name__ == "__main__":
         dd1['pred_flux_H'].append(-999)
     data_out = pd.DataFrame(dd1)
     data_out.to_csv(f"{args.output_dir}/evaluation_results.csv", index=False)
+    print(f"Saved evaluation results to {args.output_dir}/evaluation_results.csv")
