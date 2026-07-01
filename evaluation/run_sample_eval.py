added center normalization to fix bright boundary norm issues

evaluation/run_sample_eval.py
@@ -96,8 +96,11 @@ if __name__ == "__main__":
             print('Model produced empty or nan valued images')
             continue
         # shape is (3, 64, 64) for the median image
-        out1 = normalize_unit(s_median)
-        im_norm = normalize_unit(fimg[6:]) # Normalize the target image (last 3 channels)
+        # out1 = normalize_unit(s_median)
+        # im_norm = normalize_unit(fimg[6:]) # Normalize the target image (last 3 channels)
+        # Trying to use center normalization instead of unit normalization for both the predicted and target images
+        out1 = normalize_center(s_median)
+        im_norm = normalize_center(fimg[6:]) # Normalize the target image (last 3 channels)
         if abs(out1).max() == np.inf or abs(im_norm).max() == np.inf:
             print('Normalization produced inf values. Skipping.')
             continue