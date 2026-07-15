4e01328c559b6bf2fa835daafc37f9e83617959b
berres2002 <berres2002@gmail.com>
2026-07-15 12:31:57 -0400

    fixes to failsafe

--- data_processing/delta/extractDC2.py
+++ data_processing/delta/extractDC2.py
@@ -181,19 +181,20 @@ if __name__ == "__main__":
                             annots['path'].append(path)
                             annots['img'].append(cutout_fname)
                             count +=1
+                            if args.save_rubin_cutouts:
+                                rubin_cutout_data, rubin_cutout_wcs = make_cutout(coadd_rubin, wcs_rubin, pos_radec=(objs['ra'][i], objs['dec'][i]), cutout_size=np.ceil(args.cutout_size*(0.11/0.2)).astype(int)) # 0.2 rubin pixel scale, 0.11 roman pixel scale, so 64 pixel cutout is ~12.8 arcsec for rubin and ~7 arcsec for roman, rounding to nearest pixel
+                                if rubin_cutout_data is not None and np.isnan(rubin_cutout_data.min())==False:
+                                    rubin_cutout_fname = f"{rubin_fname.strip('.npy').split('/')[-1]}_cut_{objs['id'][i]}_rubin.npy"
+                                    rubin_path = os.path.join(args.output, 'data', rubin_cutout_fname)
+                                    np.save(rubin_path, rubin_cutout_data.astype(np.float32))
+                                    annots['rubin_cutout_path'].append(rubin_path)
+                                    annots['rubin_cutout_img'].append(rubin_cutout_fname)
+                                else:
+                                    # print(f"Could not make Rubin cutout for source {objs['id'][i]} at ra={objs['ra'][i]}, dec={objs['dec'][i]}. Skipping this source.")
+                                    annots['rubin_cutout_path'].append('None')
+                                    annots['rubin_cutout_img'].append('None')
                             
-                        if args.save_rubin_cutouts:
-                            rubin_cutout_data, rubin_cutout_wcs = make_cutout(coadd_rubin, wcs_rubin, pos_radec=(objs['ra'][i], objs['dec'][i]), cutout_size=np.ceil(args.cutout_size*(0.11/0.2)).astype(int)) # 0.2 rubin pixel scale, 0.11 roman pixel scale, so 64 pixel cutout is ~12.8 arcsec for rubin and ~7 arcsec for roman, rounding to nearest pixel
-                            if rubin_cutout_data is not None and np.isnan(rubin_cutout_data.min())==False:
-                                rubin_cutout_fname = f"{rubin_fname.strip('.npy').split('/')[-1]}_cut_{objs['id'][i]}_rubin.npy"
-                                rubin_path = os.path.join(args.output, 'data', rubin_cutout_fname)
-                                np.save(rubin_path, rubin_cutout_data.astype(np.float32))
-                                annots['rubin_cutout_path'].append(rubin_path)
-                                annots['rubin_cutout_img'].append(rubin_cutout_fname)
-                            else:
-                                print(f"Could not make Rubin cutout for source {objs['id'][i]} at ra={objs['ra'][i]}, dec={objs['dec'][i]}. Skipping this source.")
-                                annots['rubin_cutout_path'].append('None')
-                                annots['rubin_cutout_img'].append('None')
+
                         if args.n_test is not None and count >= args.n_test:
                                 break_stop = True
                                 print(f"n_test limit ({args.n_test}) has been reached, stopping now.")
