bc6c136559e41716aecf1860fdf97b0cb33876e3
berres2002 <berres2002@gmail.com>
2026-08-20 15:29:49 -0500

    Added Asinh norm YJH dataset class, removed mult_val from reg YJH class

--- training/pytorch_dataset.py
+++ training/pytorch_dataset.py
@@ -90,10 +90,37 @@ class CustomImageDatasetYJH(Dataset):
     def __getitem__(self, idx):
         img_path = os.path.join(self.img_labels.iloc[idx,0]) # Point this to individual image paths
         # image = decode_image(img_path)
-        mult_val = 1/159.23617710583153
-        image_full = torch.from_numpy(np.load(img_path)*mult_val) # shape (9, 64, 64) for 9 channels (6 Rubin + 3 Roman) and 64x64 cutout size
+        # mult_val = 1/159.23617710583153
+        image_full = torch.from_numpy(np.load(img_path)) # shape (9, 64, 64) for 9 channels (6 Rubin + 3 Roman) and 64x64 cutout size
         # image_cond = image_full[:6] # Rubin channels as conditioning information
         image = image_full[6:] # Roman channels as
+        # image=image.unsqueeze(0)  # Add channel dimension if needed
+        image = image.to(device='cpu')  # Load .npy file as tensor
+        # image_cond = image_cond.to(device='cuda')  # Load .npy file as tensor
+        # label = self.img_labels.iloc[idx, 1]
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+        return image
+
+class CustomImageDatasetYJHAsinh(Dataset):
+    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
+        self.img_labels = pd.read_csv(annotations_file) # This file contain image filenames
+        self.img_dir = img_dir
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.img_labels)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_labels.iloc[idx,0]) # Point this to individual image paths
+        # image = decode_image(img_path)
+        # mult_val = 1/159.23617710583153
+        image_full = torch.from_numpy(_AsinhNormalize(np.load(img_path)[6:])) # shape (3, 64, 64) for 3 channels (3 Roman) and 64x64 cutout size
+        # image_cond = image_full[:6] # Rubin channels as conditioning information
+        image = image_full # Roman channels as
         # image=image.unsqueeze(0)  # Add channel dimension if needed
         image = image.to(device='cpu')  # Load .npy file as tensor
         # image_cond = image_cond.to(device='cuda')  # Load .npy file as tensor
